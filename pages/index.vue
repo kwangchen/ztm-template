938b6b558b28f6af717a9bbcbb9bc3989e5efd53
kwangchen <55238165+kwangchen@users.noreply.github.com>
2022-04-09 07:55:07 +0800

    burgers menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,43 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">The Elections Lab</h1>
-      <p class="mt-5">Campaigns, Votes and More</p>
+  <div id="header" class="bg-[url('/hero-bg.jpg')] px20">
+    <div id="menu" class="grid grid-cols-2 py-5">
+      <div id="menu-left">
+        <img src="logo.png" class="w-[150px]" alt="" />
+      </div>
+
+      <div id="menu-right">
+        <ul class="grid grid-cols-4 text-lg font-oswald text-white uppercase pt-5">
+          <li>About Us</li>
+          <li>Our Menu</li>
+          <li>Locations</li>
+          <li>Contacts</li>
+        </ul>
+      </div>
+    </div>
+    <div class="grid grid-cols-2">
+      <img src="hero-burger.png" class="pr-10 mt-10" alt="" />
+      <div>
+        <h1 class="text-8xl text-white text-centre font-lilita uppercase">
+          Classic Burger
+        </h1>
+        <h2 class="text-6xl text-white text-center font-lilita uppercase mt-5 mb-5">
+          With Onion
+        </h2>
+        <p class="text-white font-roboto font-light text-center">
+          Our signature burger now comes with onion and cheese. You can customize it to
+          your likings. Freshly made to order, all in a Brioche style bun.
+        </p>
+      </div>
+    </div>
+
+    <div id="section-1">
+      <h1>The burger tastes better when you eat it with family</h1>
+      <p>
+        Spend quality time with your family and eat an affordable, nutritious and
+        delicious meal together. Not all burgers are fast foods. All our burgers are
+        handmade to orders for you and your family members to enjoy.
+      </p>
+      <button>Explore Full Menu</button>
     </div>
   </div>
 </template>
-
