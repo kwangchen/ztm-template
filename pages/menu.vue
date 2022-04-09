2767246a977586df676c1efbb73876dde0bfb574
kwangchen <55238165+kwangchen@users.noreply.github.com>
2022-04-09 13:23:15 +0800

    item  menu

--- pages/menu.vue
+++ pages/menu.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="header" class="bg-[url('/hero-bg.jpg')] px-20">
+    <div class="bg-[url('/menu-page.jpg')] px-20">
       <div id="menu" class="grid grid-cols-2 py-5">
         <div id="menu-left">
           <img src="logo.png" class="w-[150px]" alt="" />
@@ -15,6 +15,27 @@
           </ul>
         </div>
       </div>
+      <h1 class="text-white font-oswald uppercase text-6xl text-center pt-24 pb-28">
+        Our Menu
+      </h1>
+    </div>
+    <div id="items" class="grid grid-cols-3">
+      <MenuItem v-for="b in items" :key="b.name" :item="b" />
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    this.$axios.get('items.json').then((response) => {
+      this.items = response.data.burgers;
+    });
+  },
+};
+</script>
